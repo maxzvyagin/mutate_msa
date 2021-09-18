8bff2c138a8bb7f2c015deb491a981d2553a41f4
maxzvyagin <59709404+maxzvyagin@users.noreply.github.com>
2021-09-18 15:49:57 -0700

    change to subprocess run

--- run_alphafold.py
+++ run_alphafold.py
@@ -1,15 +1,17 @@
 import os
 from tqdm import tqdm
+import subprocess
+
 
 def alphafold(path):
     # run alphafold
-    os.system('bash /opt/alphafold/run.sh -d /lambda_stor/data/hsyoo/AlphaFoldData  -o ~/mutate_msa/folding_results/ '
-              '-f {} -t 2020-05-01 -p casp14 -m model_1,'
-              'model_2,model_3,model_4,model_5 -a 0,1,3,4,5,6,7'.format(path))
+    subprocess.run(['bash', '/opt/alphafold/run.sh', '-d /lambda_stor/data/hsyoo/AlphaFoldData',
+                    '-o ~/mutate_msa/folding_results/',
+                    '-f {}'.format(path), '-t 2020-05-01', '-p casp14',
+                    '-m model_1, model_2,model_3,model_4,model_5 -a 0,1,3,4,5,6,7'])
 
 
 if __name__ == "__main__":
     files = os.listdir('mutations')
     for f in tqdm(files):
         alphafold('mutations/{}'.format(f))
-
