156c1a2f26ec97fc2d15c0a7d268f763ce5765ee
maxzvyagin <59709404+maxzvyagin@users.noreply.github.com>
2021-09-18 15:39:10 -0700

    add run alphafold cript

--- run_alphafold.py
+++ run_alphafold.py
@@ -3,7 +3,9 @@ from tqdm import tqdm
 
 def alphafold(path):
     # run alphafold
-
+    os.system('bash /opt/alphafold/run.sh -d /lambda_stor/data/hsyoo/AlphaFoldData  -o ~/mutate_msa/folding_results/ '
+              '-f {} -t 2020-05-01 -p casp14 -m model_1,'
+              'model_2,model_3,model_4,model_5 -a 0,1,3,4,5,6,7'.format(path))
 
 
 if __name__ == "__main__":
