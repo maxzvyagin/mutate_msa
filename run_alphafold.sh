6fb8d2c498900d4cdf1b69420feaa25f262b6b1e
maxzvyagin <59709404+maxzvyagin@users.noreply.github.com>
2021-09-18 18:13:00 -0700

    Update run_alphafold.sh

--- run_alphafold.sh
+++ run_alphafold.sh
@@ -2,6 +2,7 @@
 for FILE in mutations/*
 do
 	echo $FILE
+	bash /opt/alphafold/run.sh -d /lambda_stor/data/hsyoo/AlphaFoldData  -o ~/mutate_msa/alphafold_runs/ -f $FILE -t 2020-05-01 -p casp14 -m model_1,model_2,model_3,model_4,model_5 -a 0,1,2,3,4,5,6,7
 #	command2 on $OUTPUT
 #	commandN
 done
