f3effa0bb758309eb69b7b5e3b040b5940f8b581
maxzvyagin <59709404+maxzvyagin@users.noreply.github.com>
2021-09-20 09:43:58 -0700

    Update ray_run_alphafold.py

--- ray_run_alphafold.py
+++ ray_run_alphafold.py
@@ -7,7 +7,7 @@ import ray
 def alphafold(path):
     # run alphafold
     print("Running for path {}...".format(path))
-    os.system('bash run.sh -d /lambda_stor/data/hsyoo/AlphaFoldData -o ~/mutate_msa/folding_results/ -f {} -t 2020-05-01 -p casp14 -m model_1, model_2,model_3,model_4,model_5'.format(path))
+    os.system('bash run.sh -d /lambda_stor/data/hsyoo/AlphaFoldData -o ~/mutate_msa/folding_results/ -f {} -t 2020-05-01 -p casp14 -m model_1,model_2,model_3,model_4,model_5 -a 0'.format(path))
 
 
 if __name__ == "__main__":
