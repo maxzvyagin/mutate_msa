2664633e532536113675af9d948c426d156df2a8
maxzvyagin <59709404+maxzvyagin@users.noreply.github.com>
2021-09-20 09:37:26 -0700

    Update ray_run_alphafold.py

--- ray_run_alphafold.py
+++ ray_run_alphafold.py
@@ -3,11 +3,11 @@ from tqdm import tqdm
 import subprocess
 import ray
 
-@ray.remote(num_gpus=1)
+@ray.remote(num_gpus=1, num_cpus=8)
 def alphafold(path):
     # run alphafold
     print("Running for path {}...".format(path))
-    subprocess.run(['bash', '/opt/alphafold/run.sh', '-d /lambda_stor/data/hsyoo/AlphaFoldData',
+    subprocess.run(['bash', 'run.sh', '-d /lambda_stor/data/hsyoo/AlphaFoldData',
                     '-o ~/mutate_msa/folding_results/',
                     '-f {}'.format(path), '-t 2020-05-01', '-p casp14',
                     '-m model_1, model_2,model_3,model_4,model_5 -g'])
